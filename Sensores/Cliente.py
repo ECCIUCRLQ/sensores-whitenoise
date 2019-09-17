99720b70295e1809804bdae7eec27a241fde6dba
silvi0nsky <silviosalac@gmail.com>
2019-09-16 18:55:46 -0600

    cliente.py y servidor.py ahora tienen cualidad de proceso

--- Sensores/Cliente.py
+++ Sensores/Cliente.py
@@ -1,60 +1,59 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*- 
-
 import socket
+import time
 import sys
-
+import random
 from CARRETA import CARRETA
 from BUEY import BUEY
+from Utilidades import Utilidades
+from SensorId import SensorId
 
-HOST = 'localhost'
-PORT = 10005
 
 class Cliente:
 
+	def __init__(self, ip_adress, port_number): #Podria recibir el path del archivo de donde lee
+		self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+		self.server_address = (ip_adress, port_number)
+		self.frecuencia = 1
+
 	def enviar_paquete(self, carreta_enviar):
+		dato_enviar = carreta_enviar.pack_byte_array()
+		self.sock.sendto(dato_enviar, self.server_address)
 
-		# Crea el socket UDP.
-		sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+	def recibir_paquete(self):
+		dato_recibido, server = self.sock.recvfrom(4096)
+		buey_recibido = BUEY()
+		buey_recibido.unpack_byte_array(dato_recibido)
+		print(buey_recibido)
+		return buey_recibido
 
-		# Un segundo de timeout.
-		sock.settimeout(1)
+	def send_recv_loop(self):
+		# Resuelve la ambiguedad de ACK no numerado(pérdida de paquetes). Figura 5.10, Página 275, Libro León García.
+		# Reenvía el paquete carreta hasta que reciba la confirmación por parte de un paquete buey.
+		# ToDo: Carreta se crea con datos del archivo si hay datos si no se queda esperando
+		carreta = CARRETA(random.getrandbits(8), Utilidades.get_unix_time(), SensorId([1,0,0,1]), 2, 0)
+		
+		# Envia datos todo el tiempo!
+		while True:
+			# Envia los datos.
+			self.enviar_paquete(carreta)
+			try:
+				# Recibe un paquete BUEY
+				buey = 	self.recibir_paquete()
+				if carreta.rand_id == buey.rand_id:
+					# ToDo: Carreta se crea con datos del archivo si hay datos si no se queda esperando
+					carreta = CARRETA(random.getrandbits(8), Utilidades.get_unix_time(), SensorId([1,0,0,1]), 2, 0)
+				else:
+					print("CLIENTE -  Error: Rid carreta = %s, Rid buey = %s.\n" % (carreta.rand_id, buey.rand_id))	
+			except socket.timeout:
+				print("CLIENTE - Buey no recibido en el intervalo definido, reenviando carreta con Rid = %s.\n" % carreta.rand_id)
+			time.sleep(self.frecuencia)
 
-		server_address = (HOST, PORT)
+def iniciar_cliente():
+	# Se crea un cliente que se conecta al socket(address) indicado por linea de comandos
+	cliente = Cliente(sys.argv[1], (int)(sys.argv[2]))
+	cliente.sock.settimeout(3)
+	cliente.send_recv_loop()
 
-		# Archivo para guardar los datos generados por el sensor.
-		file = open("DatosCliente.txt", "a")
-
-		try:
-			file.write("%s\n\n" % carreta_enviar.__str__())
-
-			haya_confirmado_buey = False
-			dato_enviar = carreta_enviar.pack_byte_array()
-
-			rid_carreta_enviada = carreta_enviar.rand_id
-			buey_recibido = BUEY()
-			
-			# Resuelve la ambiguedad de ACK no numerado(pérdida de paquetes). Figura 5.10, Página 275, Libro León García.
-			# Reenvía el paquete carreta hasta que reciba la confirmación por parte de un paquete buey.
-			while not haya_confirmado_buey:
-				
-				# Envia los datos.
-				sock.sendto(dato_enviar, server_address)
-				
-				try:
-					# Recibe un paquete BUEY.
-					dato_recibido, server = sock.recvfrom(4096)
-					buey_recibido.unpack_byte_array(dato_recibido)
-					rid_buey_recibido = buey_recibido.rand_id
-					print(buey_recibido)	
-
-					if rid_carreta_enviada == rid_buey_recibido:
-						haya_confirmado_buey = True
-					else:
-						print("CLIENTE -  Error: Rid carreta = %s, Rid buey = %s.\n" % (rid_carreta_enviada, rid_buey_recibido))
-						
-				except socket.timeout: 
-					print ("CLIENTE - Buey no recibido en el intervalo definido, reenviando carreta con Rid = %s.\n" % rid_carreta_enviada)
-		finally:
-			file.close()
-			sock.close()
+iniciar_cliente()
--- Sensores/Servidor.py
+++ Sensores/Servidor.py
@@ -1,32 +1,19 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*- 
-
 import socket
 import sys
 import time
-
 from CARRETA import CARRETA
 from BUEY import BUEY
 
-HOST = ''
-PORT = 10005
-
 class Servidor:
 
-	@classmethod
-	def recibir(cls):
+	def __init__(self, ip_adress, port_number): #Podria recibir el path del archivo de donde lee
+		self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+		self.my_address = (ip_adress, port_number)
+		self.client_address = 0
 
-		# Crea el socket UDP.
-		sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-
-		# Configura el puerto y la direccion donde estaran esperando datos.
-		server_address = (HOST, PORT)
-		sock.bind(server_address)
-
-		# Archivo para guardar los datos que recibe.
-		file = open("DatosServidor.txt", "a")
-
-		# Se asegura que la conexión con el cliente aún está abierta.
+	def recibir(self):
 		connected = True
 
 		rid_ultima_carreta_procesada = -1
@@ -34,25 +21,21 @@ class Servidor:
 		while connected:
 
 			# Recibe los datos (data) y la direccion desde donde se enviaron (address)
-			dato_recibido, address = sock.recvfrom(4096)
-
+			dato_recibido, self.client_address = self.sock.recvfrom(4096)
 			# Convierte los datos en un paquete CARRETA para generar un paquete BUEY
 			carreta_recibida = CARRETA()
 			carreta_recibida.unpack_byte_array(dato_recibido)
 
 			# Cierra la conexión si en el paquete CARRETA viene la señal end_connection
-			if carreta_recibida.type == 2:
-				connected = False
-
-			rid_carreta_recibda = carreta_recibida.rand_id
-
+			#if carreta_recibida.type == 2:
+			#	connected = False
 			# Resuelve la ambiguedad ACk perdido (duplicación de paquetes).Figura 5.9b, Página 275, Libro León García.
-			if rid_ultima_carreta_procesada != rid_carreta_recibda:
+			if rid_ultima_carreta_procesada != carreta_recibida.rand_id:
 
 				# Escribe el dato recibido en el archivo
 				print("SERVIDOR - Rid carreta = %s escrito en el archivo." % carreta_recibida.rand_id)
-				file.write("%s\n\n" % carreta_recibida.__str__())
-				rid_ultima_carreta_procesada = rid_carreta_recibda
+				#file.write("%s\n\n" % carreta_recibida.__str__())
+				rid_ultima_carreta_procesada = carreta_recibida.rand_id
 				
 			else:
 				print("SERVIDOR - La carreta con rid = %s, ya fue recibida anteriormente." % carreta_recibida.rand_id)
@@ -61,13 +44,18 @@ class Servidor:
 			buey_confirmacion.rand_id = carreta_recibida.rand_id
 			buey_confirmacion.sensor_id = carreta_recibida.sensor_id
 
-			buey_enviar = buey_confirmacion.pack_byte_array()
+			datos_enviar = buey_confirmacion.pack_byte_array()
 
-			if buey_enviar:
-				sent = sock.sendto(buey_enviar, address)
+			if datos_enviar:
+				self.sock.sendto(datos_enviar, self.client_address)
 				
-		sock.close()
+		self.sock.close()
 		print("Socket closed due to 'end_connection' signal, server no longer listening")
-		file.close()
-		print("Data file is closed")
 		return
+
+def iniciar_servidor():
+	servidor = Servidor(sys.argv[1], (int)(sys.argv[2]));
+	servidor.sock.bind(servidor.my_address)
+	servidor.recibir()
+
+iniciar_servidor()
