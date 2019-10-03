e4ca54b4e1c10014fc67d7a4eb5514345a46c40b
José Frutos <jfrutos@mdgdevelopers.com>
2019-10-03 09:58:24 -0600

    Prototipo Administrador de Memoria

---
+++ Sensores/AdministradorMemoria.py
@@ -0,0 +1,22 @@
+
+class AdministradorMemoria:
+
+	@classmethod
+	def write(cls, direccion_fisica, dato, tamano_dato):
+		return 0
+
+	@classmethod
+	def read_mp(cls, direccion_fisica):
+		return 0
+
+	@classmethod
+	def read_mv(cls, direccion_fisica):
+		return 0
+
+	@classmethod
+	def read_all(cls, direccion_fisica):
+		return 0
+
+	@classmethod
+	def malloc(cls):
+		return 0
