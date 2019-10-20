3c57cea0fc3c8c46dce511efb9a44332816cb21a
José Frutos <jfrutos@mdgdevelopers.com>
2019-10-20 14:03:28 -0600

    - Casos de test.

--- Sensores/Test.py
+++ Sensores/Test.py
@@ -13,14 +13,36 @@ print(AdministradorMemoria.malloc())
 
 print(AdministradorMemoria.malloc())
 
-AdministradorMemoria.write("2x00000", b"A")
+s = "Prueba String"
+s = bytes(s, 'utf-8') 
+datosA = pack("I%ds" % (len(s),), len(s), s)
 
-AdministradorMemoria.write("0x00000", b"B")
+AdministradorMemoria.write("2x00000", datosA)
+
+ss = "Prueba String 2"
+ss = bytes(ss, 'utf-8') 
+datosB = pack("I", len(ss)) + ss
+
+AdministradorMemoria.write("0x00000", datosB)
 
 print(AdministradorMemoria.read(2))
 
 print(AdministradorMemoria.read(1))
 
-AdministradorMemoria.write("3x00000", b"C")
+sss = "Prueba String 3"
+sss = bytes(sss, 'utf-8') 
+datosC = pack("I", len(sss)) + sss
 
-AdministradorMemoria.write("1x00000", b"D")
+AdministradorMemoria.write("3x00000", datosC)
+
+ssss = "Prueba String 4"
+ssss = bytes(ssss, 'utf-8') 
+datosD = pack("I", len(ssss)) + ssss
+
+AdministradorMemoria.write("1x00000", datosD)
+
+print(AdministradorMemoria.read(3))
+
+print(AdministradorMemoria.read(1))
+
+print(AdministradorMemoria.read(0))
