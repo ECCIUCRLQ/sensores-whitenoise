5e00c4eabb7c7528d8cc7f846a355345d4d1b063
Pablo Ruiz <paceruva@MacBook-Pro-de-Pablo.local>
2019-10-20 17:29:49 -0600

    Implementación Interfaz

---
+++ Sensores/Interfaz.py
@@ -0,0 +1,30 @@
+from AdministradorMemoria import AdministradorMemoria
+from TablaControlConfig import TablaControlConfig
+from SensorId import SensorId
+
+class Intefaz:
+    tabla_control = []
+    tabla_offset = []
+    PAGE_SIZE = 76800
+
+    @classmethod
+    def guardar(cls, dirLog, message):
+        dirFisica = str(dirLog)
+        offset = str(cls.tabla_offset[dirLog])
+        direccion = dirFisica.zfill(5) + 'x' + offset.zfill(5)
+        AdministradorMemoria.write(direccion, message)
+        cls.tabla_offset[dirLog] += cls.tabla_control[dirLog].tamano_dato
+
+    @classmethod
+    def leer(cls, dirLog):
+        datos = []
+        for pagina in cls.tabla_control[dirLog].paginas:
+            datos.append(AdministradorMemoria.read(paginas))
+        return datos
+
+    @classmethod
+    def malloc_maravilloso(cls, sensor_id, tamano_dato):
+        pagina = AdministradorMemoria.malloc(tamano_dato)
+        cls.tabla_control.append(new TablaControlConfig(sensor_id, [paginas], tamano_dato))
+        cls.tabla_offset.append(0)
+        return tabla_control
