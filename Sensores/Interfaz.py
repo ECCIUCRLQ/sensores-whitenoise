559e1d06b3e6ac0875cb9c3faad6b18e934c4f01
Pablo Ruiz <paceruva@MacBook-Pro-de-Pablo.local>
2019-10-20 18:23:21 -0600

    Corrección de errores Interfaz.py

--- Sensores/Interfaz.py
+++ Sensores/Interfaz.py
@@ -10,15 +10,15 @@ class Intefaz:
     @classmethod
     def guardar(cls, dirLog, message):
         offset = str(cls.tabla_offset[dirLog])
-        if offset >= PAGE_SIZE:
-            paginaNueva = Administrados.malloc(tamano_dato)
+        if offset >= PAGE_SIZE-1:
+            paginaNueva = AdministradorMemoria.malloc(tamano_dato)
             tabla_control[dirLog].paginas.append(paginaNueva)
-            offset = 0
+            offset = '0'
+            cls.tabla_offset[dirLog] = 0
         dirFisica = str(max(tabla_control[dirLog].paginas))
         direccion = dirFisica.zfill(5) + 'x' + offset.zfill(5)
         AdministradorMemoria.write(direccion, message)
         cls.tabla_offset[dirLog] += cls.tabla_control[dirLog].tamano_dato
-
 
     @classmethod
     def leer(cls, dirLog):
